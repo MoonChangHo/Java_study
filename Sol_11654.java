6b9d6a2ce6238b17c7a225d22554866ea75681b0
MoonChangHo <87019291+MoonChangHo@users.noreply.github.com>
2021-12-28 22:15:01 +0900

    Add files via upload

---
+++ Sol_11654.java
@@ -0,0 +1,18 @@
+package test.problem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Sol_11654 {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br=
+                new BufferedReader(new InputStreamReader(System.in));
+        String as= br.readLine();
+        int a= as.charAt(0);
+        System.out.println(a);
+
+
+
+    }
+}
