17dfdc08e869a97bd85a805f7be6ef094cf6fcc3
MoonChangHo <87019291+MoonChangHo@users.noreply.github.com>
2022-01-01 23:48:13 +0900

    Add files via upload

--- src/test/Sol_2675.java
+++ src/test/Sol_2675.java
@@ -1,11 +1,13 @@
 package test.problem;
 
-import jdk.swing.interop.SwingInterOpUtils;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Sol_2675 {
-    public static void main(String[] args) {
+    public static void main(String[] args)throws IOException {
+        /*
         Scanner a= new Scanner(System.in);
         int testCase=a.nextInt();
         int num;
@@ -20,6 +22,24 @@ public class Sol_2675 {
             }
             System.out.println("");
         }
+        */
+
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder a=new StringBuilder();
+
+        int T=Integer.parseInt(br.readLine());
+        for(int i=0;i<T;i++){
+            String[] str=br.readLine().split(" ");
+            int num=Integer.parseInt(str[0]);
+
+            for(int k=0;k<str[1].length();k++){
+                for (int j=0;j<num;j++){
+                    a.append(str[1].charAt(k));
+                }
+            }
+            a.append("\n");
+        }
+        System.out.print(a);
 
     }
 }
