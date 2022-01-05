3a49179bdfc51a5d1da169974d37b9f3f10d3201
MoonChangHo <87019291+MoonChangHo@users.noreply.github.com>
2022-01-05 23:14:38 +0900

    Add files via upload

---
+++ src/test/Bronze/Sol_10998.java
@@ -0,0 +1,13 @@
+package test.problem.Bronze;
+
+import java.util.Scanner;
+
+public class Sol_10998 {
+        public static void main(String[] args) {
+            Scanner s=new Scanner(System.in);
+            int a=s.nextInt();
+            int b=s.nextInt();
+
+            System.out.print((a*b));
+        }
+}
